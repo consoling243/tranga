64fd01068a343bcc7377e734200752f0d83ff5b0
consoling243 <github.consoling243@passmail.com>
2026-02-08 21:28:36 -0500

    commit number 22

--- API/MangaConnectors/Comix.cs
+++ API/MangaConnectors/Comix.cs
@@ -259,7 +259,7 @@ public class Comix : MangaConnector
 
         while (page <= lastPage)
         {
-            string apiUrl = $"https://comix.to/api/v2/manga/{hashId}/chapters?limit=100&page={page}&order[number]=desc";
+            string apiUrl = $"https://comix.to/api/v2/manga/{hashId}/chapters?limit=100&page={page}&order[number]=asc";
 
             HttpResponseMessage response = downloadClient
                 .MakeRequest(apiUrl, RequestType.Default)
